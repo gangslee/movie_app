ac1139303c894882d6d4ef6ee005fe173bb98532
gangslee <37660333+gangslee@users.noreply.github.com>
2020-06-26 10:28:32 +0900

    Set Movie Component
    Set Movie Component

--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({ id, year, summary, poster, title }) {
-  return <h5>{title}</h5>;
+function Movie({year, summary, poster, title }) {
+  return (
+    <div class="movie">
+      <img src={poster} alt={title} title={title}/>
+      <div class="movie_data">
+        <h3 class="movie_title">{title}</h3>
+        <h5 class="movie_year">{year}</h5>
+        <p class="movie_summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
 
 Movie.propTypes = {
